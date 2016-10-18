9de014389bdb339ec3cf2b34cd52834c2a696f64
M.Vainauskas <mindelis1@gmail.com>
2016-10-18 15:54:31 +0100

    Added basic login template and started working on getting bootstrap template styling to work

--- webApp.py
+++ webApp.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return  render_template('index.html')#return function returns premade text with the name added to the end.
+def home():
+    return  render_template('home.html')#return function returns premade text with the name added to the end.
 
 @app.route("/signup")
 def signup():
@@ -15,9 +15,7 @@ def signup():
 def login():
     return  render_template('login.html')
 
-@app.route("/")
-def home():
-    return  render_template('home.html')
+
 #Managed to get the templates routing working thanks to: http://flask.pocoo.org/docs/0.11/quickstart/#routing
 
 
