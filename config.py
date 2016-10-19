3b55e683e32e1fdc1b0b25bb78d2e1d53a60e5b3
M.Vainauskas <mindelis1@gmail.com>
2016-10-19 21:24:38 +0100

    returned configuration to same file for now. Still working towards setting up a database in flask

--- config.py
+++
@@ -1,6 +0,0 @@
-#Configuration settings for the application 
-DATABASE = '/webApp.db'#database location url
-USERNAME = 'admin'#user name for admin
-PASSWORD = 'admin'#password for admin
-SECRET_KEY = 'secret18'#secret key will be needed to secure client side connections to the server
-DEBUG = True #Switch on debug mode so its easy to see changes to app on the fly
--- webApp.py
+++ webApp.py
@@ -1,17 +1,43 @@
 #Import necessary libraries and a database for application to work
+import os
 import sqlite3
 from flask import Flask, render_template, url_for, redirect, g
-from flask_sqlalchemy import SQLAlchemy#database connector for flask?
 from contextlib import closing
 
+
 app = Flask(__name__)
-app.config.from_object('config')#configuration for the app comes from config.py file in the same folder
-db = SQLAlchemy(app)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+	#Configuration settings for the application 
+	DATABASE= os.path.join(app.root_path, 'webApp.db'),#database location url
+	USERNAME='admin',#user name for admin
+	PASSWORD='admin',#password for admin
+	SECRET_KEY='secret18',#secret key will be needed to secure client side connections to the server
+	DEBUG = True #Switch on debug mode so its easy to see changes to app on the fly
+))
 
 
+#Connect to the DB
 def connect_db():#Get the connection to DB sorted out
-    return sqlite3.connect(app.config['DATABASE'])
+    rw = sqlite3.connect(app.config['DATABASE'])
+    rw.row_factory = sqlite3.Row
+    return rw
 
+#Initiate call to DB
+def get_db():
+	if not hasattr(g, 'webApp_db'):
+		g.webApp_db = connect_db()
+	return g.webApp_db
+	
+#Teard down connection when it is not needed anymore
+@app.teardown_appcontext
+def close_db(error):
+    
+    if hasattr(g, 'webApp_db'):
+        g.webApp_db.close()
+
+#Originally adapted from http://flask.pocoo.org/docs/0.11/tutorial/dbinit/#tutorial-dbinit
 #Initialise the DB for Application
 def init_db():
     with closing(connect_db) as db:
@@ -21,8 +47,7 @@ def init_db():
         
 @app.cli.command('initdb')
 def initdb_command():    
-    init_db()
-    print ('Initialized the database.')
+    init_db()    
 
 @app.route("/")#Initial page that user sees when web app loads
 def login():
@@ -39,4 +64,5 @@ def home():
 
 
 if __name__ == "__main__":
+
     app.run()
