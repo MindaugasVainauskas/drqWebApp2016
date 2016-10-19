d5e69feccd171d79a4b21f7e4f499841331c72c3
M.Vainauskas <mindelis1@gmail.com>
2016-10-19 17:52:15 +0100

    Redone some of the file structure. Added config.py file to keep configuration of web app file to keep it modular

--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
-#Users table for registered users
+--Users table for registered users
 drop table if exists users
     create table users(
       id integer primary key autoincrement,
       name varchar(20) not null,
       surname varchar(20) not null,
-      password text null,
-      phone varchar(15) not null,
+      password varchar(20) null,      
       email varchar(20) not null
     );
     
-#Contacts table where users will keep their contacts
+--Contacts table where users will keep their contacts
 drop table if exists contacts
     create table contacts(
-        cid integer primary key autoincrement,
-        uid integer foreign key reference table users(id),
+        cid integer autoincrement,
+        usid integer foreign key reference table users(id),
         cname varchar(20) not null,
         csurname varchar(20) not null,
         cphone varchar(15) not null,
-        cemail varchar(20) not null
+        cemail varchar(20) not null,
+		primary key (cid, usid)
     );
--- webApp.py
+++ webApp.py
@@ -1,18 +1,12 @@
 #Import necessary libraries and a database for application to work
 import sqlite3
 from flask import Flask, render_template, url_for, redirect, g
+from flask_sqlalchemy import SQLAlchemy#database connector for flask?
 from contextlib import closing
 
-
-#Configuration settings for the application 
-DATABASE = '/tmp/webApp.db'#database location url
-USERNAME = 'admin'#user name for admin
-PASSWORD = 'admin'#password for admin
-SECRET_KEY = 'secret18'#secret key will be needed to secure client side connections to the server
-DEBUG = True #Switch on debug mode so its easy to see changes to app on the fly
-
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object('config')#configuration for the app comes from config.py file in the same folder
+db = SQLAlchemy(app)
 
 
 def connect_db():#Get the connection to DB sorted out
@@ -29,24 +23,19 @@ def init_db():
 def initdb_command():    
     init_db()
     print ('Initialized the database.')
-        
 
-
-
-@app.route("/")
+@app.route("/")#Initial page that user sees when web app loads
 def login():
-    return  render_template('login.html')#return function returns premade text with the name added to the end.
+    return  render_template('login.html')
 
-@app.route("/signup")
+@app.route("/signup")#Page to register new users
 def signup():
     return  render_template('signUp.html')
 
-@app.route("/home")
+@app.route("/home")#Page that users see if they have signed in or registered
 def home():
     return  render_template('home.html')
 
-
-#Managed to get the templates routing working thanks to: http://flask.pocoo.org/docs/0.11/quickstart/#routing
 
 
 if __name__ == "__main__":
