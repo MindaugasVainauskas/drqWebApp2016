d143cb02c5fdb4eec008dd7f15e6139a959d8dc3
M.Vainauskas <mindelis1@gmail.com>
2016-11-02 17:16:32 +0000

    Got the registration page working. MongoDB database now stores user details and password hash with salt used to generate it.

--- webApp.py
+++ webApp.py
@@ -1,11 +1,11 @@
 # Import necessary libraries and a database for application to work
 
-import flask as fl
-from flask import render_template, url_for, redirect, g, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_pymongo import PyMongo
+from flask_scrypt import generate_random_salt, generate_password_hash, check_password_hash
 
 
-app = fl.Flask(__name__)
+app = Flask(__name__)
 
 
 app.config['MONGO_DBNAME'] = 'usersDB'
@@ -23,31 +23,40 @@ def login():
 
 @app.route("/signup", methods=['POST', 'GET'])  # Page to register new users
 def signup():
-    if request.method == "POST":
-        username = request.values['inputUserName']
-        email = request.values['inputEmail']
-        password = request.values['inputPassword']
-        # create user account
-        createuser(username, email, password)
-        return render_template('home.html')
+    if request.method == 'POST':
+        salt = generate_random_salt()  # generate random salt for the new users password
+
+        # check if user exists in DB
+        user_exists = mongo.db.usersDB.find_one({'username': request.form['inputUsername']})
+
+        if user_exists is None:
+            username = request.form['inputUsername']  # take in the username from form input
+            email = request.form['inputEmail']  # take in the email from form selection
+            password = request.form['inputPassword']  # take in the password from form selection
+            pass_hash = generate_password_hash(password, salt)  # generate password hash that will get stored in the DB
+            # create user account
+            createuser(username, email, pass_hash, salt)
+            return redirect(url_for('home'))
+        return 'User with such name exists!!!'
     else:
         return render_template('signUp.html')
 
 
-# this method will insert new users into user_table in the database
-def createuser(username, email, password):
+# this method will insert new users into the collection in the database
+def createuser(username, email, password, salt):
     # get user details from http page and put into mongoDB
     mongo.db.usersDB.insert({
-        username: "name"
-        , email: "email"
-        , password: "password"
+        'username': username  # insert username into document
+        , 'email': email  # insert email
+        , 'password': password  # insert password hash
+        , 'salt': salt  # insert salt that was used to generate password hash. To be used with login later
     })
 
 
-@app.route('/home/<username>')  # Page that users see if they have signed in or registered
-def home(username):
-    user = mongo.db.users.find_one_or_404({'name': username})
-    return render_template('home.html', user=user)
+@app.route('/home')  # Page that users see if they have signed in or registered
+def home():
+    # user = mongo.db.users.find_one({'username': username})
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
