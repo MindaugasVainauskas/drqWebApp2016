536a2b8cd32a707804e79a00cbe6961a0c1b52ad
M.Vainauskas <mindelis1@gmail.com>
2016-11-02 13:28:50 +0000

    Started replacement of Sqlite with MongoDB. New Database will offer easier manipulation of collections in the applications and will offer more flexibility than RDB's can offer. Cleaned out unnecessary files for now. might bring some of them back later if needed.

--- config.py
+++ config.py
@@ -1,63 +1,20 @@
-#This is the configuration file for the database that will be used by the application
-#If database does not exist(first time running/database has been deleted) then this will create it.
+# This is the configuration file for the database that will be used by the application
+# If database does not exist(first time running/database has been deleted) then this will create it.
 
-import sqlite3
 
-#file path to the database is defined
-DATABASE = 'app_db/appDB.db'
+from flask.ext.pymongo import PyMongo
 
-#setup the database for users who will use the application
+mongo = PyMongo()
+# file path to the database is defined
+DATABASE = 'c:\data\db\webAppDB'
+
+# setup the database for users who will use the application
 def setup_db():
-	db = sqlite3.connect(DATABASE)
-	cur = db.cursor()
 
-	#if the database does not exist get it created
-	cur.execute("CREATE TABLE IF NOT EXISTS user_table(id integer primary key autoincrement, name varchar(20),	surname varchar(20), password varchar(192))")
-	db.commit()
+    mongo.db.createCollection("users")
 
-	#insert some dummy user data for testing purposes when DB is created.
-	cur.execute("SELECT COUNT(*) FROM user_table")
-	if cur.fetchall()[0][0] == 0:
-		cur.execute('INSERT INTO user_table(name, surname, password) VALUES("admin", "adminas", "Admin1")')		
-		db.commit()		
+
+
 
 if __name__ == "__main__":
-	setup_db()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	  
+    setup_db()
--- webApp.py
+++ webApp.py
@@ -1,68 +1,54 @@
-#Import necessary libraries and a database for application to work
-import os
-import sqlite3
+# Import necessary libraries and a database for application to work
+
 import flask as fl
 from flask import render_template, url_for, redirect, g, request
-from contextlib import closing
-
-import appMethods
+from flask_pymongo import PyMongo
 
 
 app = fl.Flask(__name__)
 
-DATABASE = 'app_db/appDB.db'
 
-#Connect to the DB
-def connect_db():#Get the connection to DB sorted out
-    rw = fl.g._database = sqlite3.connect(DATABASE)    
-    return rw
-
-#Initiate call to DB
-def get_db():
-    db = getattr(fl.g, '_database', None)
-    if db is not None:
-        db = fl.g._database = sqlite3.connect(DATABASE)
-        return db
-    
-	
-#Teard down connection when it is not needed anymore
-@app.teardown_appcontext
-def close_db(exception):
-    db = getattr(fl.g, "_database", None)
-    if db is not None:
-        db.close()
+app.config['MONGO_DBNAME'] = 'usersDB'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/usersDB'
 
 
-#Routing functions
-@app.route("/")#Initial page that user sees when web app loads
+mongo = PyMongo(app)
+
+
+# Routing functions
+@app.route("/")  # Initial page that user sees when web app loads
 def login():
-    return  render_template('login.html')
+    return render_template('login.html')
 
-@app.route("/signup", methods=['POST', 'GET'])#Page to register new users
+
+@app.route("/signup", methods=['POST', 'GET'])  # Page to register new users
 def signup():
     if request.method == "POST":
-        _name = request.args.get('inputName')
-        _surname = request.args.get('inputSurname')
-        _password = request.args.get('inputPassword')
-        createuser(_name, _surname, _password)
+        username = request.values['inputUserName']
+        email = request.values['inputEmail']
+        password = request.values['inputPassword']
+        # create user account
+        createuser(username, email, password)
         return render_template('home.html')
     else:
-        return  render_template('signUp.html')
+        return render_template('signUp.html')
 
 
-#this method will insert new users into user_table in the database
-def createuser(name, surname, password):
-    db = connect_db()
-    cur = db.cursor()    
-    cur.execute('INSERT INTO user_table(name, surname, password) VALUES (?, ?, ?)', (name, surname, password))
-    db.commit()    
-    db.close()
-
-@app.route("/home")#Page that users see if they have signed in or registered
-def home():    
-    return  render_template('home.html')
+# this method will insert new users into user_table in the database
+def createuser(username, email, password):
+    # get user details from http page and put into mongoDB
+    mongo.db.usersDB.insert({
+        username: "name"
+        , email: "email"
+        , password: "password"
+    })
 
 
+@app.route('/home/<username>')  # Page that users see if they have signed in or registered
+def home(username):
+    user = mongo.db.users.find_one_or_404({'name': username})
+    return render_template('home.html', user=user)
 
-if __name__ == "__main__":    
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
